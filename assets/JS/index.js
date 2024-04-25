28c6ad89c8acfd26a6249e4e63437e6d9ae92020
Agus-amr <161776041+Agus-amr@users.noreply.github.com>
2024-04-25 15:32:11 +0000

    >>fcion suma ok

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -2,14 +2,14 @@ console.log("hoolaa!")
 
 //calculadora
 
-let firstNum  = prompt("ingrese un nro")
-let secondNum = prompt("ingrese otro nro")
+let firstNum  = Number(prompt("ingrese un nro"))
+let secondNum = Number(prompt("ingrese otro nro"))
 
 //Esta función realiza un suma de los parametros n1 y n2
 function sumaDeNros (n1, n2) {
-    return (n1 + n2)
+    return n1 + n2
 }
 
 let sumaTotal = sumaDeNros (firstNum, secondNum)
 
-document.writeln (`<h2> La suma total es de : ${sumaTotal} </h2>`)
+document.write (`<h2> La suma total es de : ${sumaTotal} </h2>`)
